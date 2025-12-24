76ddb1bfa73a1a7f9439062a05a6c82962804829
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-12-24 22:01:59 +0100

    chore: mutex single thread fail

---
+++ src/bin/mutex_single_thread.rs
@@ -0,0 +1,15 @@
+use std::{sync::Mutex, thread};
+
+fn main() {
+    let treasure_chest: Mutex<i32> = Mutex::new(0);
+
+    let handle = thread::spawn(move || {
+        let mut data = treasure_chest.lock().unwrap();
+        *data += 100;
+        println!("Value inside the lock: {}", *data);
+    });
+
+    // let final_data = treasure_chest.lock().unwrap(); // value borrowed here after move
+    // println!("Final value: {}", *final_data);
+    handle.join().ok();
+}
