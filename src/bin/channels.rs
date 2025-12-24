84d0309395aeb77be4b75824b8151c330f43ce9c
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-12-24 19:34:54 +0100

    chore: channels

---
+++ src/bin/channels.rs
@@ -0,0 +1,18 @@
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::thread;
+
+fn main() {
+    // Create a channel
+    let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+
+    // Spawn thread and move transmitter (tx) into it
+    thread::spawn(move || {
+        let val = String::from("hi from thread");
+        tx.send(val).unwrap();
+        // val is moved. We can't use it here anymore.
+    });
+
+    // Block main thread until a message is received
+    let received = rx.recv();
+    println!("Got: {:?}", received);
+}
